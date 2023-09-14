174fe0cbaafda894346a09cfae676f962276ed4e
Gaby-r-f <104916814+Gaby-r-f@users.noreply.github.com>
2023-09-14 12:21:44 -0400

    Add files via upload

---
+++ formulario de pago/main.js
@@ -0,0 +1,5 @@
+/*  <input type="text" placeholder="code"  maxlength="4" onkeyup="if (this.value.length == this.getAttribute('maxlength')) input2.focus()" /> 
+
+        <input type="text" id="input2" placeholder="code" maxlength="4" onkeyup="if (this.value.length == this.getAttribute('maxlength')) input3.focus()" /> 
+
+        <input type="text" id="input3" placeholder="code" maxlength="4" onkeyup="if (this.value.length == this.getAttribute('maxlength')) " />  */
